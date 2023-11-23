e0b60cce9e719f073fd2b3844ac73ab036914427
camrohlof <camrohlof@gmail.com>
2023-11-22 21:17:42 -0500

    inital commit

---
+++ dungeon_crawl/lib/dungeon_crawl/character.ex
@@ -0,0 +1,20 @@
+defmodule DungeonCrawl.Character do
+  @type t :: %DungeonCrawl.Character{
+          name: String.t(),
+          description: String.t(),
+          hit_points: non_neg_integer,
+          max_hit_points: non_neg_integer,
+          attack_description: String.t(),
+          damage_range: Range.t()
+        }
+  defstruct name: nil,
+            description: nil,
+            hit_points: 0,
+            max_hit_points: 0,
+            attack_description: nil,
+            damage_range: nil
+
+  defimpl String.Chars do
+    def to_string(character), do: character.name
+  end
+end
